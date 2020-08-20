87cd158c4eeeda5c5a2fe5ab0e33a8aef6c3aa43
Jim <jsmith774@gmail.com>
2020-08-20 12:47:41 -0400

    GET bookmarks/:id IN PROGRESS

--- src/bookmarks/bookmarks-router.js
+++ src/bookmarks/bookmarks-router.js
@@ -9,99 +9,81 @@ const bookmarksRouter = express.Router();
 const bodyParser = express.json();
 
 bookmarksRouter
-    .route('/')
-    .get((req, res, next) => {
-        BookmarksService.getAllBookmarks(
-            req.app.get('db')
-        )
-        .then(bookmarks => res.json(bookmarks))
-        .catch(next);
-    })
-    .post(bodyParser, (req, res) => {
-        const { title, url, description = '', rating } = req.body;
-        
-        //check that title url and rating are passed in
-        if (!title) {
-            logger.error(`Title is required`);
-            return res
-                .status(400)
-                .send('Invalid data');
-        }
-        if (!url) {
-            logger.error(`URL is required`);
-            return res
-                .status(400)
-                .send('Invalid data');
-        }
-        if (!rating) {
-            logger.error(`Rating is required`);
-            return res
-                .status(400)
-                .send('Invalid data');
-        }
-        // check that rating is passed as a number between 0 and 5
-        if (!Number.isInteger(rating) || rating < 0 || rating > 5) {
-            logger.error(`Rating ${rating} supplied is invalid`);
-            return res
-                .status(400)
-                .send('Rating must be a number between 0 and 5');
-        }
-        // check that url at least starts with http:// or https://
-        if (!url.startsWith('http://') && !url.startsWith('https://')) {
-            logger.error(`URL ${url} supplied is invalid`);
-            return res
-                .status(400)
-                .send('URL must begin with http(s)://');
-        }
+  .route('/')
+  .get((req, res, next) => {
+    BookmarksService.getAllBookmarks(req.app.get('db'))
+      .then((bookmarks) => res.json(bookmarks))
+      .catch(next);
+  })
+  .post(bodyParser, (req, res) => {
+    const { title, url, description = '', rating } = req.body;
 
-        const id = uuid();
-        const bookmark = {
-            id,
-            title,
-            url,
-            description,
-            rating
-        };
+    //check that title url and rating are passed in
+    if (!title) {
+      logger.error(`Title is required`);
+      return res.status(400).send('Invalid data');
+    }
+    if (!url) {
+      logger.error(`URL is required`);
+      return res.status(400).send('Invalid data');
+    }
+    if (!rating) {
+      logger.error(`Rating is required`);
+      return res.status(400).send('Invalid data');
+    }
+    // check that rating is passed as a number between 0 and 5
+    if (!Number.isInteger(rating) || rating < 0 || rating > 5) {
+      logger.error(`Rating ${rating} supplied is invalid`);
+      return res.status(400).send('Rating must be a number between 0 and 5');
+    }
+    // check that url at least starts with http:// or https://
+    if (!url.startsWith('http://') && !url.startsWith('https://')) {
+      logger.error(`URL ${url} supplied is invalid`);
+      return res.status(400).send('URL must begin with http(s)://');
+    }
 
-        bookmarks.push(bookmark);
+    const id = uuid();
+    const bookmark = {
+      id,
+      title,
+      url,
+      description,
+      rating,
+    };
 
-        logger.info(`Bookmark with id ${id} created`);
+    bookmarks.push(bookmark);
 
-        res.status(201)
-            .location(`http://localhost:${PORT}/bookmarks/${id}`)
-            .json(bookmark);
-    });
+    logger.info(`Bookmark with id ${id} created`);
+
+    res
+      .status(201)
+      .location(`http://localhost:${PORT}/bookmarks/${id}`)
+      .json(bookmark);
+  });
 
 bookmarksRouter
-    .route('/:id')
-    .get((req, res) => {
-        const { id } = req.params;
+  .route('/:id')
+  .get((req, res, next) => {
+    const { id } = req.params;
 
-        const bookmark = bookmarks.find(bm => bm.id == id);
+    BookmarksService.getBookmarkById(req.app.get('db'), id)
+      .then((bookmark) => res.json(bookmark))
+      .catch(next);
+  })
+  .delete((req, res) => {
+    const { id } = req.params;
 
-        if(!bookmark) {
-            logger.error(`Bookmark with id ${id} not found`);
-            return res.status(404).send('Not Found');
-        }
+    const bookmarkIndex = bookmarks.findIndex((bm) => bm.id == id);
 
-        res.json(bookmark);
-    })
-    .delete((req, res) => {
-        const { id } = req.params;
+    if (bookmarkIndex === -1) {
+      logger.error(`Bookmark with id ${id} not found`);
+      return res.status(404).send('Not Found');
+    }
 
-        const bookmarkIndex = bookmarks.findIndex(bm => bm.id == id);
+    const deleteBook = bookmarks.splice(bookmarkIndex, 1);
 
-        if(bookmarkIndex === -1) {
-            logger.error(`Bookmark with id ${id} not found`);
-            return res.status(404).send('Not Found');
-        }
-
-        const deleteBook = bookmarks.splice(bookmarkIndex, 1);
-
-        logger.info(`Bookmark with id ${id} deleted.`)
-        res
-            .status(204)
-            .end();
-    });
+    logger.info(`Bookmark with id ${id} deleted.`);
+    res.status(204).end();
+  });
 
 module.exports = bookmarksRouter;
--- test/bookmarks-endpoints.spec.js
+++ test/bookmarks-endpoints.spec.js
@@ -4,37 +4,39 @@ const makeBookmarksArray = require('./bookmarks-fixtures');
 const supertest = require('supertest');
 
 describe.only('Bookmarks Endpoints', function () {
-    let db;
+  let db;
 
-    before('make knex instance', () => {
-        db = knex({
-            client: 'pg',
-            connection: process.env.TEST_DB_URL,
-        });
-        app.set('db', db);
+  before('make knex instance', () => {
+    db = knex({
+      client: 'pg',
+      connection: process.env.TEST_DB_URL,
     });
+    app.set('db', db);
+  });
 
-    after('disconnect from db', () => db.destroy());
+  after('disconnect from db', () => db.destroy());
 
-    before('clean the table', () => db('bookmarks').truncate());
+  before('clean the table', () => db('bookmarks').truncate());
 
-    afterEach('clean the table', () => db('bookmarks').truncate());
+  afterEach('clean the table', () => db('bookmarks').truncate());
 
-    describe('GET /bookmarks', () => {
-        context('Given there are bookmarks in the database', () => {
-            const testBookmarks = makeBookmarksArray();
+  describe('GET /bookmarks', () => {
+    //todo run with no data
 
-            beforeEach('insert test data', () => {
-                return db
-                    .into('bookmarks')
-                    .insert(testBookmarks);
-            });
+    context('Given there are bookmarks in the database', () => {
+      const testBookmarks = makeBookmarksArray();
 
-            it('responds with 200 and all of the bookmarks', () => {
-                return supertest(app)
-                    .get('/bookmarks')
-                    .expect(200, testBookmarks);
-            });
-        });
+      beforeEach('insert test data', () => {
+        return db.into('bookmarks').insert(testBookmarks);
+      });
+
+      it('responds with 200 and all of the bookmarks', () => {
+        return supertest(app).get('/bookmarks').expect(200, testBookmarks);
+      });
+
+      it('resonds with 200 and the specified bookmark', () => {
+        return supertest(app).get('/bookmarks/2').expect(200, testBookmarks[1]);
+      });
     });
+  });
 });
