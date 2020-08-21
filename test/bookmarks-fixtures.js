0f7bd80514d0393c1b913b24aa7cfd09e4c7774e
nhemerling <65194792+nhemerling@users.noreply.github.com>
2020-08-21 11:27:30 -0700

    add xss sanitization to all endpoints and passing tests to confirm (#1)

--- test/bookmarks-fixtures.js
+++ test/bookmarks-fixtures.js
@@ -24,6 +24,30 @@ function makeBookmarksArray() {
         "brings together the world's largest community of developers.",
     },
   ];
-}
+};
 
-module.exports = makeBookmarksArray;
+function makeMaliciousBookmark() {
+  const maliciousBookmark = {
+      id: 911,
+      title: 'Naughty naughty very naughty <script>alert("xss");</script>',
+      url: 'http://www.facebook.com',
+      description: `Bad image <img src="https://url.to.file.which/does-not.exist" onerror="alert(document.cookie);">. But not <strong>all</strong> bad.`,
+      rating: 4,
+  };
+
+  const expectedBookmark = {
+      ...maliciousBookmark,
+      title: 'Naughty naughty very naughty &lt;script&gt;alert(\"xss\");&lt;/script&gt;',
+      description: `Bad image <img src="https://url.to.file.which/does-not.exist">. But not <strong>all</strong> bad.`
+  };
+
+  return {
+      maliciousBookmark,
+      expectedBookmark
+  };
+};
+
+module.exports = {
+  makeMaliciousBookmark,
+  makeBookmarksArray
+};
